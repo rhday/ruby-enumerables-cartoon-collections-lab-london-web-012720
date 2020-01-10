5e4638266483e9c93438ed65be9bd657b5d1ccf0
Ruari Day <ruariday@gmail.com>
2020-01-10 14:03:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,10 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |planeteer| planeteer.capitalize + "!"}
 end
 
-def long_planeteer_calls(planeteer_calls)
-
-end
+def long_planteer_calls(planeteer_calls)
+  planeteer_calls.any? do |call|
+  call.length > 4
+  end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
